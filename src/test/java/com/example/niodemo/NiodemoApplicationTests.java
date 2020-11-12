060b7765fbe3d5f2068cc6de739bfd1217153ccc
huangshylc <1007954564@qqc.com>
2020-11-12 18:17:00 +0800

    nio示例工程，初始化

---
+++ src/test/java/com/example/niodemo/NiodemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.niodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NiodemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
